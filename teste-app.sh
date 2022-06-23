89e35b8b512eaadc63153b97769e18d795eb72ca
vitorbgama <vitorbrandao.ti@outlook.com>
2022-06-23 09:08:11 -0300

    adcionando o teste-app.sh

---
+++ teste-app.sh
@@ -0,0 +1,14 @@
+#/bin/bahs
+RESULT="`wget -qO- http://localhost:8090`"
+wget -q loacalhost:8090
+if [$? -eq 0]
+then
+  echo 'ok - serviço no ar!'
+elif[[ $RESULT == *"Number"*]]
+then
+  echo 'ok - number os visits'
+  echo $RESULT
+else
+  echo 'not ok - number of visit'
+  exit 1
+fi
