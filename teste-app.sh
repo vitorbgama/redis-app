fed81dbc7abf236ba371908e6767b2f4fef7c84f
vitorbgama <vitorbrandao.ti@outlook.com>
2022-07-01 10:08:06 -0300

    corrigindo bugs

--- teste-app.sh
+++ teste-app.sh
@@ -1,13 +1,15 @@
-#/bin/bahs
-RESULT="`wget -qO- http://localhost:8090`"
+#!/bin/bahs
+#shellcheck shell=bash
+
+RESULT="$(wget -qO- http://localhost:8090)"
 wget -q localhost:8090
-if [ $? -eq 0 ]
+if wget -q localhost:8090
 then
   echo "ok - serviço no ar!"
 elif [[ $RESULT == *"Number"* ]]
 then
   echo "ok - number os visits"
-  echo $RESULT
+  echo "$RESULT"
 else
   echo "not ok - number of visit"
   exit 1
