f5bb595ef476c4aa66931a1441dfba56692e8710
vitorbgama <vitorbrandao.ti@outlook.com>
2022-07-01 11:40:51 -0300

    melhoria no script de teste

--- Jenkinsfile
+++ Jenkinsfile
@@ -22,5 +22,10 @@ pipeline{
         sh './teste-app.sh'
       }
     }
+    stage('Shutdown dos containers de teste'){
+      steps{
+        sh 'docker-compose down'
+      }
+    }
   }
 }
--- teste-app.sh
+++ teste-app.sh
@@ -2,13 +2,16 @@
 
 RESULT="$(wget -qO- http://localhost:8090)"
 wget -q localhost:8090
+
 if wget -q localhost:8090
 then
   echo "ok - serviço no ar!"
+
 elif [[ $RESULT == *"Number"* ]]
 then
   echo "ok - number os visits"
   echo "$RESULT"
+
 else
   echo "not ok - number of visit"
   exit 1
